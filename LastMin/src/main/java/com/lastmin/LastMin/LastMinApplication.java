426182567eec68acd2029786caec70d0101242a4
Niamh Byrne <niamhb196@gmail.com>
2022-07-24 12:45:11 +0100

    first commit in collegeproject repo

---
+++ LastMin/src/main/java/com/lastmin/LastMin/LastMinApplication.java
@@ -0,0 +1,13 @@
+package com.lastmin.LastMin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastMinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastMinApplication.class, args);
+	}
+
+}
